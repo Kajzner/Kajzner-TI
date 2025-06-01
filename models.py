4cf451f3df5ee180146ee0206910ffe5aa8cd63d
Kajzner <85492149+Kajzner@users.noreply.github.com>
2025-06-01 19:56:37 +0200

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, Integer, String, Float
 from db_config import ORMBaseModel
 from pydantic import BaseModel
 
+# TODO Wzorując się na poniższych przykładach, zdefiniuj odpowienie modele w swojej aplikacji.
+
 class Person(ORMBaseModel):
     __tablename__ = "person"
     id = Column(Integer, primary_key=True, index=True)
@@ -9,15 +11,20 @@ class Person(ORMBaseModel):
     last_name = Column(String, nullable=False)
     person_type_id = Column(Integer, index=True, nullable=False)
     age = Column(Integer, nullable=False)
-    latitude = Column(Float, nullable=True)     # NOWE
-    longitude = Column(Float, nullable=True)    # NOWE
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+
 
 class PersonCreate(BaseModel):
     first_name: str
     last_name: str
     person_type_id: int
     age: int
+    latitude: float | None = None
+    longitude: float | None = None
 
-class PositionUpdate(BaseModel):  # NOWE
+
+class PositionUpdate(BaseModel):
     latitude: float
     longitude: float
+
